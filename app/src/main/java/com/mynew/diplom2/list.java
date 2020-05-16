a2b84014162f88c0ece8d66eb1d68a5188c9acb7
murbook <dimamurugov@gmail.com>
2020-05-16 20:47:14 +0300

    my project

---
+++ app/src/main/java/com/mynew/diplom2/list.java
@@ -0,0 +1,16 @@
+package com.mynew.diplom2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class list extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+
+
+    }
+}
